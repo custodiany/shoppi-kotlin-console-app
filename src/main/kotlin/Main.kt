73144acfa61e3429c651a7bade9cec91d4da488a
DESKTOP-88A6AV6\kerberos <hyeongseokgim@gmail.com>
2022-10-14 18:09:23 +0900

    fisrt_

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 fun main(args: Array<String>) {
-    println("이름을 넣어주세요!")
-    val name = readLine()
-    println("안녕하세요 $name 님! 반갑습니다.")
+    println("안녕하세요, Shoopi에 오신 것을 환영합니다!")
+    println("쇼핑을 계속 하시려면 이름을 입력해주세요! :)")
+    val username = readLine()
+
+    println("""
+        감사합니다. 반갑습니다! $username 님!
+        원하시는 카테고리를 입력해주세요! 
+        ===============================================
+    """.trimIndent())
+
 }
